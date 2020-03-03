2d19f0675b5162a5dda8ae153725ef51a5fbdd19
Fabian Herb <fabian@herb-clan.de>
2020-03-03 07:47:18 +0100

    Move GcdTaskDispatcher to molecular-util

--- molecular/util/GcdAsyncFileLoader.h
+++ molecular/util/GcdAsyncFileLoader.h
@@ -2,7 +2,7 @@
 
 MIT License
 
-Copyright (c) 2019 Fabian Herb
+Copyright (c) 2019-2020 Fabian Herb
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -28,7 +28,7 @@ SOFTWARE.
 
 #include <molecular/util/BlobFunctionTask.h>
 #include <functional>
-#include "GcdTaskDispatcher.h"
+#include <molecular/util/GcdTaskDispatcher.h>
 #include <stdexcept>
 #include <sys/stat.h>
 #include <iostream>
